12e2b3c04250acade3f6a3c448649d01479f4395
abhijeet8900 <abhijeet8900@gmail.com>
2023-11-27 15:15:18 +0530

    - Swtich to network with better band

--- utils.py
+++ utils.py
@@ -1,3 +1,4 @@
+from decimal import Decimal
 import os
 from logger import log
 
@@ -5,18 +6,24 @@ from logger import log
 def getNetworkStrength(signal: str) -> int:
     return int(signal.replace("%", ""))
 
+def hasBetterBand(current:str = '', new: str = ''):
+    currentBand = Decimal(current.replace(" GHz",""))
+    newBand = Decimal(new.replace(" GHz",""))
+    return newBand > currentBand
 
 def getBestNetwork(currentNetwork: dict, allNetworks: list[dict], knownNetworks: list):
     currentSSID = currentNetwork.get("Signal")
     currentStrength = getNetworkStrength(currentSSID)
+    currentBand = currentNetwork.get("Band")
     bestNetwork = None
     for network in allNetworks:
         SSID = network.get("SSID")
         isKnownNetwork = knownNetworks.count(SSID) > 0
         networkStrength = getNetworkStrength(network.get("Signal"))
+        networkBand = network.get("Band")
         if isKnownNetwork:
             if SSID != currentSSID:
-                if networkStrength > currentStrength:
+                if networkStrength > currentStrength or hasBetterBand(currentBand, networkBand): # or better band 
                     if bestNetwork:
                         if networkStrength > getNetworkStrength(bestNetwork.get("Signal")):
                             bestNetwork = network
